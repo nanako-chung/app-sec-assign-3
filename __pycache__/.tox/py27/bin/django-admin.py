a662cf17d09fe68a38e2704c24f4cd5c626fc702
nanako-chung <nanako.s.chung@gmail.com>
2019-12-04 16:02:27 -0500

    done

--- __pycache__/.tox/py27/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/nanakochung/Desktop/School/Senior Year/App Sec/assign-2/.tox/py27/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
